a3e5bdc0e7d202d9ebe13036ff4c2e0f31c8ecdc
Gabriel Cardoso Girarde <gabrielcardosogirarde@gmail.com>
2024-11-07 13:54:24 -0300

    chore: deploy configs

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   app.enableVersioning();
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
